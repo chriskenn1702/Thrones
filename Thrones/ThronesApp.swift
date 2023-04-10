b02f97f970415c575c75b1730216b1a8cde7a294
Christopher Kennedy <kennednu@bc.edu>
2023-04-10 16:35:11 -0400

    Initial Commit

---
+++ Thrones/ThronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThronesApp.swift
+//  Thrones
+//
+//  Created by Christopher Kennedy on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
